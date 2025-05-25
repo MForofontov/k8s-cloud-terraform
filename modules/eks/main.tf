80c9f88db22fd1a0547c7d42c63a30e9ff2de9ca
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-05-25 17:38:18 +0100

    initialize EKS module with main Terraform configuration

---
+++ modules/eks/main.tf
@@ -0,0 +1,67 @@
+// EKS Cluster Terraform Module
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  version = var.kubernetes_version
+
+  tags = var.tags
+}
+
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    max_size     = var.node_max_size
+    min_size     = var.node_min_size
+  }
+
+  instance_types = var.node_instance_types
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-eks-cluster-role"
+
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+// Optionally, output kubeconfig for use with kubectl
+resource "local_file" "kubeconfig" {
+  content  = aws_eks_cluster.this.kubeconfig[0].raw_kubeconfig
+  filename = "${path.module}/kubeconfig_${var.cluster_name}"
+}
