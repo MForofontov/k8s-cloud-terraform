d659c1925b537091641718b2f49bbbb2d8ef0327
Mykyta Forofontov <81178208+MForofontov@users.noreply.github.com>
2025-05-26 16:06:40 +0100

    feat: update AKS outputs for improved clarity and additional information

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,34 +1,47 @@
-// AKS Cluster Outputs
+# Outputs for AKS module
 
-output "cluster_id" {
-  description = "The ID of the AKS cluster"
+# Cluster information
+output "id" {
+  description = "The Kubernetes Managed Cluster ID"
   value       = azurerm_kubernetes_cluster.this.id
 }
 
-output "cluster_name" {
-  description = "The name of the AKS cluster"
+output "name" {
+  description = "The Kubernetes Managed Cluster name"
   value       = azurerm_kubernetes_cluster.this.name
 }
 
-output "cluster_resource_group_name" {
-  description = "The resource group where the AKS cluster is deployed"
+output "resource_group_name" {
+  description = "The resource group name where the AKS cluster was created"
   value       = azurerm_kubernetes_cluster.this.resource_group_name
 }
 
-output "cluster_version" {
-  description = "The Kubernetes version of the AKS cluster"
+output "location" {
+  description = "The location where the AKS cluster was created"
+  value       = azurerm_kubernetes_cluster.this.location
+}
+
+output "kubernetes_version" {
+  description = "The Kubernetes version used by the AKS cluster"
   value       = azurerm_kubernetes_cluster.this.kubernetes_version
 }
 
-output "kube_config_raw" {
-  description = "Raw Kubernetes config to be used by kubectl and other compatible tools"
-  value       = azurerm_kubernetes_cluster.this.kube_config_raw
+output "dns_prefix" {
+  description = "The DNS prefix of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.this.dns_prefix
+}
+
+# Access configuration
+output "kube_config" {
+  description = "The full kubeconfig for the AKS cluster"
+  value       = azurerm_kubernetes_cluster.this.kube_config
   sensitive   = true
 }
 
-output "kube_config_path" {
-  description = "Path to the generated kubeconfig file"
-  value       = local_file.kubeconfig.filename
+output "kube_config_raw" {
+  description = "The raw kubeconfig in YAML format"
+  value       = azurerm_kubernetes_cluster.this.kube_config_raw
+  sensitive   = true
 }
 
 output "host" {
@@ -38,40 +51,61 @@ output "host" {
 }
 
 output "client_certificate" {
-  description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster"
+  description = "The client certificate for authentication"
   value       = azurerm_kubernetes_cluster.this.kube_config[0].client_certificate
   sensitive   = true
 }
 
 output "client_key" {
-  description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster"
+  description = "The client key for authentication"
   value       = azurerm_kubernetes_cluster.this.kube_config[0].client_key
   sensitive   = true
 }
 
 output "cluster_ca_certificate" {
-  description = "Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster"
+  description = "The cluster CA certificate"
   value       = azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate
   sensitive   = true
 }
 
-output "node_resource_group" {
-  description = "The auto-generated resource group which contains the resources for this managed Kubernetes cluster"
-  value       = azurerm_kubernetes_cluster.this.node_resource_group
+# Node pool information
+output "default_node_pool" {
+  description = "Information about the default node pool"
+  value = {
+    name                = azurerm_kubernetes_cluster.this.default_node_pool[0].name
+    vm_size             = azurerm_kubernetes_cluster.this.default_node_pool[0].vm_size
+    node_count          = azurerm_kubernetes_cluster.this.default_node_pool[0].node_count
+    enable_auto_scaling = azurerm_kubernetes_cluster.this.default_node_pool[0].enable_auto_scaling
+    min_count           = azurerm_kubernetes_cluster.this.default_node_pool[0].min_count
+    max_count           = azurerm_kubernetes_cluster.this.default_node_pool[0].max_count
+    os_disk_size_gb     = azurerm_kubernetes_cluster.this.default_node_pool[0].os_disk_size_gb
+    vnet_subnet_id      = azurerm_kubernetes_cluster.this.default_node_pool[0].vnet_subnet_id
+    zones               = azurerm_kubernetes_cluster.this.default_node_pool[0].zones
+    max_pods            = azurerm_kubernetes_cluster.this.default_node_pool[0].max_pods
+  }
 }
 
-output "principal_id" {
-  description = "The principal ID of the system assigned identity of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.this.identity[0].principal_id
+output "additional_node_pools" {
+  description = "Map of additional node pools created"
+  value = {
+    for name, pool in azurerm_kubernetes_cluster_node_pool.additional_pools : name => {
+      id                  = pool.id
+      vm_size             = pool.vm_size
+      node_count          = pool.node_count
+      enable_auto_scaling = pool.enable_auto_scaling
+      min_count           = pool.min_count
+      max_count           = pool.max_count
+      os_disk_size_gb     = pool.os_disk_size_gb
+      os_disk_type        = pool.os_disk_type
+      mode                = pool.mode
+      priority            = pool.priority
+    }
+  }
 }
 
-output "kubelet_identity" {
-  description = "The kubelet identity of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.this.kubelet_identity
-}
-
+# Network information
 output "network_profile" {
-  description = "Network profile of the AKS cluster"
+  description = "Network configuration for the AKS cluster"
   value = {
     network_plugin     = azurerm_kubernetes_cluster.this.network_profile[0].network_plugin
     network_policy     = azurerm_kubernetes_cluster.this.network_profile[0].network_policy
@@ -79,36 +113,62 @@ output "network_profile" {
     dns_service_ip     = azurerm_kubernetes_cluster.this.network_profile[0].dns_service_ip
     docker_bridge_cidr = azurerm_kubernetes_cluster.this.network_profile[0].docker_bridge_cidr
     outbound_type      = azurerm_kubernetes_cluster.this.network_profile[0].outbound_type
-    pod_cidr           = azurerm_kubernetes_cluster.this.network_profile[0].pod_cidr
+    load_balancer_sku  = azurerm_kubernetes_cluster.this.network_profile[0].load_balancer_sku
   }
 }
 
-output "additional_node_pools" {
-  description = "Map of additional node pools and their properties"
-  value       = { for k, v in azurerm_kubernetes_cluster_node_pool.additional : k => {
-    id                  = v.id
-    name                = v.name
-    vm_size             = v.vm_size
-    os_disk_size_gb     = v.os_disk_size_gb
-    os_disk_type        = v.os_disk_type
-    os_type             = v.os_type
-    enable_auto_scaling = v.enable_auto_scaling
-    node_count          = v.node_count
-    min_count           = v.min_count
-    max_count           = v.max_count
-    node_labels         = v.node_labels
-    node_taints         = v.node_taints
-    zones               = v.zones
-    mode                = v.mode
-  }}
+# Identity information
+output "identity" {
+  description = "Identity configuration for the AKS cluster"
+  value = {
+    type         = azurerm_kubernetes_cluster.this.identity[0].type
+    principal_id = azurerm_kubernetes_cluster.this.identity[0].principal_id
+    tenant_id    = azurerm_kubernetes_cluster.this.identity[0].tenant_id
+  }
 }
 
-output "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics Workspace"
-  value       = var.enable_log_analytics_workspace ? azurerm_log_analytics_workspace.aks[0].id : null
+# Add-on statuses
+output "addon_profiles" {
+  description = "Status of AKS add-ons"
+  value = {
+    http_application_routing_enabled = azurerm_kubernetes_cluster.this.http_application_routing_enabled
+    azure_policy_enabled             = azurerm_kubernetes_cluster.this.azure_policy_enabled
+    oms_agent_enabled                = var.enable_log_analytics
+    key_vault_secrets_provider       = var.enable_key_vault_secrets_provider
+    microsoft_defender_enabled       = var.enable_microsoft_defender
+  }
 }
 
-output "log_analytics_workspace_name" {
-  description = "The name of the Log Analytics Workspace"
-  value       = var.enable_log_analytics_workspace ? azurerm_log_analytics_workspace.aks[0].name : null
+# Cluster FQDN
+output "fqdn" {
+  description = "The FQDN of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.this.fqdn
+}
+
+# Cluster private FQDN
+output "private_fqdn" {
+  description = "The private FQDN of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.this.private_fqdn
+}
+
+# Cluster portal URL
+output "portal_fqdn" {
+  description = "The portal FQDN of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.this.portal_fqdn
+}
+
+# Auto-scaler profile
+output "auto_scaler_profile" {
+  description = "Auto-scaler profile configuration for the AKS cluster"
+  value = {
+    balance_similar_node_groups      = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].balance_similar_node_groups
+    max_graceful_termination_sec     = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].max_graceful_termination_sec
+    scale_down_delay_after_add       = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scale_down_delay_after_add
+    scale_down_delay_after_delete    = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scale_down_delay_after_delete
+    scale_down_delay_after_failure   = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scale_down_delay_after_failure
+    scan_interval                    = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scan_interval
+    scale_down_unneeded              = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scale_down_unneeded
+    scale_down_unready               = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scale_down_unready
+    scale_down_utilization_threshold = azurerm_kubernetes_cluster.this.auto_scaler_profile[0].scale_down_utilization_threshold
+  }
 }
